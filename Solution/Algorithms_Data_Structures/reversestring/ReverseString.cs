60c8f7629b17e1bf72345c3871eb26d763daf265
Roberto Freire Ferraz Passos <roberto.passos@radixeng.com.br>
2021-12-28 16:10:03 -0300

    ReverseString passed

--- Solution/Algorithms_Data_Structures/reversestring/ReverseString.cs
+++ Solution/Algorithms_Data_Structures/reversestring/ReverseString.cs
@@ -3,7 +3,17 @@
     public static class ReverseString
     {
         public static string Reverse(string input) {
-            return input;
+            char[] inputAsCharArray = input.ToCharArray();
+            int inputAsCharArrayLength = inputAsCharArray.Length;
+            char[] outputAsCharArray = new char[inputAsCharArrayLength];
+            int j = 0;
+
+            for (int i = inputAsCharArrayLength - 1; i >= 0; --i)
+            {
+                outputAsCharArray[j] = inputAsCharArray[i];
+                j++;
+            }
+            return new string(outputAsCharArray);
         }
     }
 }
