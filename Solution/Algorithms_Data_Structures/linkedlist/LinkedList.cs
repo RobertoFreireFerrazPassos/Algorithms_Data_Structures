7f1fff0e028aba7c22b9be0d89df286eb2ccca71
Roberto Freire Ferraz Passos <roberto.passos@radixeng.com.br>
2021-12-29 12:11:22 -0300

    Implementing LinkedList

---
+++ Solution/Algorithms_Data_Structures/linkedlist/LinkedList.cs
@@ -0,0 +1,31 @@
+﻿namespace Algorithms_Data_Structures
+{
+    public class LinkedList
+    {
+        public LinkedNode Head { get; private set; }
+
+        public LinkedList() 
+        {
+            Head = null;
+        }
+
+        public void InsertFirst(object data)
+        {
+            var headCopy = Head;
+            Head = new LinkedNode(data, Head);
+        }
+            
+    }
+
+    public class LinkedNode
+    {
+        public object Data;
+        public LinkedNode Next;
+
+        public LinkedNode(object data, LinkedNode next)
+        {
+            Data = data;
+            Next = next;
+        }
+    }
+}
---
+++ Solution/tests/LinkedListTest.cs
@@ -0,0 +1,19 @@
+﻿using Algorithms_Data_Structures;
+using Xunit;
+
+namespace tests
+{
+    public class LinkedListTest
+    {
+        [Fact]
+        public void MustAppendsNodeToTheStartOfTheList()
+        {
+            var list = new LinkedList();
+            list.InsertFirst(1);
+            Assert.Equal(1,list.Head.Data);
+            list.InsertFirst(2);
+            Assert.Equal(2,list.Head.Data);
+            Assert.Equal(1, list.Head.Next.Data);
+        }
+    }
+}
