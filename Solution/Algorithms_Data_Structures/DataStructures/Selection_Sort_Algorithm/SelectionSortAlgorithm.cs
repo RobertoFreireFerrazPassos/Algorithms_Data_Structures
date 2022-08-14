de3a65284b8ee69c705798432d4162234968926b
Roberto Freire <robertofreireferrazpassos@gmail.com>
2022-08-14 14:28:56 -0300

    Create a faster solution for selection sort

--- Solution/Algorithms_Data_Structures/DataStructures/Selection_Sort_Algorithm/SelectionSortAlgorithm.cs
+++ Solution/Algorithms_Data_Structures/DataStructures/Selection_Sort_Algorithm/SelectionSortAlgorithm.cs
@@ -6,31 +6,30 @@ namespace Algorithms_Data_Structures.DataStructures.Selection_Sort_Algorithm
     {
         public static int[] SelectionSort(int[] input)
         {
-            var inputAsList = new List<int>(input);
             var inputLength = input.Length;
-            var result = new int[inputLength];
             var currentPosition = 0;
 
-            while (currentPosition < inputLength)
+            while (currentPosition < inputLength - 1)
             {
-                var currentIndex = 0;
-                var currentMinValue = inputAsList[currentIndex];
+                var currentIndex = currentPosition;
+                var currentMinValue = input[currentIndex];
 
-                for (int i = 1; i < inputAsList.Count; i++)
+                for (int i = currentPosition + 1; i < input.Length; i++)
                 {
-                    if (inputAsList[i] < currentMinValue)
+                    if (input[i] < currentMinValue)
                     {
-                        currentMinValue = inputAsList[i];
+                        currentMinValue = input[i];
                         currentIndex = i;
                     }
                 }
 
-                result[currentPosition] = currentMinValue;
-                inputAsList.RemoveAt(currentIndex);
+                input[currentIndex] = input[currentPosition];
+                input[currentPosition] = currentMinValue;
+
                 currentPosition++;
             }
 
-            return result;
+            return input;
         }
 
         public static int[] SelectionSort_Solution2(int[] input)
---
+++ Solution/Benchmark/Benchmarks/SortAlgorithmsBenchmark.cs
@@ -0,0 +1,23 @@
+﻿using Algorithms_Data_Structures.DataStructures.Selection_Sort_Algorithm;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmark.Benchmarks
+{
+    /*
+        |         Method |      Mean |    Error |   StdDev |
+        |--------------- |----------:|---------:|---------:|
+        |           Sort |  41.20 ns | 0.947 ns | 1.197 ns |
+        | Sort_Solution2 | 215.91 ns | 4.330 ns | 6.868 ns |
+    */
+
+    public class SortAlgorithmsBenchmark
+    {
+        private int[] input = new int[] { 64, 25, 25, 12, 22, 11, 25 };
+
+        [Benchmark]
+        public int[] Sort() => SortAlgorithms.SelectionSort(input);
+
+        [Benchmark]
+        public int[] Sort_Solution2() => SortAlgorithms.SelectionSort_Solution2(input);
+    }
+}
