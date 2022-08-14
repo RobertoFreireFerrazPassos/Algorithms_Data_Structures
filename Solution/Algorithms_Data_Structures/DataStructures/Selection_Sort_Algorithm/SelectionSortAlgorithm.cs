8ade61330398b28dc4a95eff44ad4dfe94f85db7
Roberto Freire <robertofreireferrazpassos@gmail.com>
2022-08-14 13:59:54 -0300

    Added more tests for selection sort and fix bug

--- Solution/Algorithms_Data_Structures/DataStructures/Selection_Sort_Algorithm/SelectionSortAlgorithm.cs
+++ Solution/Algorithms_Data_Structures/DataStructures/Selection_Sort_Algorithm/SelectionSortAlgorithm.cs
@@ -9,6 +9,35 @@ namespace Algorithms_Data_Structures.DataStructures.Selection_Sort_Algorithm
             var inputAsList = new List<int>(input);
             var inputLength = input.Length;
             var result = new int[inputLength];
+            var currentPosition = 0;
+
+            while (currentPosition < inputLength)
+            {
+                var currentIndex = 0;
+                var currentMinValue = inputAsList[currentIndex];
+
+                for (int i = 1; i < inputAsList.Count; i++)
+                {
+                    if (inputAsList[i] < currentMinValue)
+                    {
+                        currentMinValue = inputAsList[i];
+                        currentIndex = i;
+                    }
+                }
+
+                result[currentPosition] = currentMinValue;
+                inputAsList.RemoveAt(currentIndex);
+                currentPosition++;
+            }
+
+            return result;
+        }
+
+        public static int[] SelectionSort_Solution2(int[] input)
+        {
+            var inputAsList = new List<int>(input);
+            var inputLength = input.Length;
+            var result = new int[inputLength];
             var currentPosition = 0;            
 
             while (currentPosition < inputLength)
@@ -18,7 +47,7 @@ namespace Algorithms_Data_Structures.DataStructures.Selection_Sort_Algorithm
 
                 for (int i = 1; i < inputAsList.Count; i++)
                 {
-                    if (inputAsList[i] < inputAsList[i - 1])
+                    if (inputAsList[i] < currentMinValue)
                     {
                         currentMinValue = inputAsList[i];
                         currentIndex = i;
--- Solution/tests/Algorithms/SelectionSortAlgorithmTest.cs
+++ Solution/tests/Algorithms/SelectionSortAlgorithmTest.cs
@@ -8,9 +8,32 @@ namespace tests
     {
         [Theory]
         [InlineData("64, 25, 25, 12, 22, 11, 25", "11, 12, 22, 25, 25, 25, 64")]
+        [InlineData("1, 25, 7, 25, 12, 22, 11, 70", "1, 7, 11, 12, 22, 25, 25, 70")]
+        [InlineData("11, 5","5, 11")]
+        [InlineData("3", "3")]
+        [InlineData("3, 5", "3, 5")]
+        [InlineData("1, 2, 3", "1, 2, 3")]
+        [InlineData("3, 2, 1", "1, 2, 3")]
+        [InlineData("3, 0, 3", "0, 3, 3")]
         public void Sort(string array, string expectedOutput)
         {
             var result = SortAlgorithms.SelectionSort(array.CreateIntArray());
+
+            Assert.Equal(expectedOutput, result.CreateString());
+        }
+
+        [Theory]
+        [InlineData("64, 25, 25, 12, 22, 11, 25", "11, 12, 22, 25, 25, 25, 64")]
+        [InlineData("1, 25, 7, 25, 12, 22, 11, 70", "1, 7, 11, 12, 22, 25, 25, 70")]
+        [InlineData("11, 5", "5, 11")]
+        [InlineData("3", "3")]
+        [InlineData("3, 5", "3, 5")]
+        [InlineData("1, 2, 3", "1, 2, 3")]
+        [InlineData("3, 2, 1", "1, 2, 3")]
+        [InlineData("3, 0, 3", "0, 3, 3")]
+        public void Sort_Solution2(string array, string expectedOutput)
+        {
+            var result = SortAlgorithms.SelectionSort_Solution2(array.CreateIntArray());
 
             Assert.Equal(expectedOutput, result.CreateString());
         }
