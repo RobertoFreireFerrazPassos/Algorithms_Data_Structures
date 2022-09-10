1293f30227c2863642abc08c8e727e50f0b2cf20
Roberto Freire <robertofreireferrazpassos@gmail.com>
2022-09-10 15:05:32 -0300

    apply single responsability adding more functions

--- Solution/Algorithms_Data_Structures/Algorithms/SortByFrequency/SortByFrequency.cs
+++ Solution/Algorithms_Data_Structures/Algorithms/SortByFrequency/SortByFrequency.cs
@@ -1,7 +1,5 @@
-﻿using Algorithms_Data_Structures.Algorithms.Sort;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Algorithms_Data_Structures.Extensions;
-using System;
 
 namespace Algorithms_Data_Structures.Algorithms.SortByFrequency
 {
@@ -22,10 +20,11 @@ namespace Algorithms_Data_Structures.Algorithms.SortByFrequency
         public static int[] Sort(int[] input) //2, 5, 2, 8, 5, 6, 8, 8
         {
             var lentgh = input.Length;
+            var copy = input.CreateCopyIntArray();
 
             // sort
             //2, 2, 5, 5, 6, 8, 8, 8
-            (var sortedInput, var copy) = input.SortCopy();
+            var sortedInput = input.SortIntArray();
 
             // create list of frequency
             //2, 2
@@ -34,7 +33,7 @@ namespace Algorithms_Data_Structures.Algorithms.SortByFrequency
             //8, 3
             var listFrequency = CreateListFrequency(sortedInput, copy, lentgh);
 
-            // sort list of frequency by frequency
+            // sort list of frequency by frequency. It must maitain order in case of same frequency
             //8, 3
             //2, 2
             //5, 2
@@ -44,72 +43,65 @@ namespace Algorithms_Data_Structures.Algorithms.SortByFrequency
             // Create new array
             //8, 8, 8, 2, 2, 5, 5, 6
             return CreateResult(listFrequency, copy, lentgh);
+        }
 
-            List<ItemFrequency> CreateListFrequency(int[] sortedInput, int[] copy, int lentgh)
+        private static List<ItemFrequency> CreateListFrequency(int[] sortedInput, int[] copy, int lentgh)
+        {
+            var listFrequency = new List<ItemFrequency>();
+            var value = sortedInput[0];
+            var frequency = 1;
+
+            for (int i = 1; i < lentgh; i++)
             {
-                var listFrequency = new List<ItemFrequency>();
-                var value = sortedInput[0];
-                var frequency = 1;
-
-                for (int i = 1; i < lentgh; i++)
+                if (value != sortedInput[i])
                 {
-                    if (value != sortedInput[i])
-                    {
-                        listFrequency.Add(new ItemFrequency(frequency, copy.GetIndexValue(value)));
-                        value = sortedInput[i];
-                        frequency = 1;
-                    }
-                    else
-                    {
-                        frequency++;
-                    }
+                    listFrequency.Add(new ItemFrequency(frequency, copy.GetIndexValue(value)));
+                    value = sortedInput[i];
+                    frequency = 1;
                 }
-                listFrequency.Add(new ItemFrequency(frequency, copy.GetIndexValue(value)));
+                else
+                {
+                    frequency++;
+                }
+            }
+            listFrequency.Add(new ItemFrequency(frequency, copy.GetIndexValue(value)));
 
-                return listFrequency;
+            return listFrequency;
+        }
+
+        private static int[] CreateResult(List<ItemFrequency> listFrequency, int[] copy, int lentgh)
+        {
+            var result = new int[lentgh];
+            var resultIndex = 0;
+
+            foreach (var itemFrequency in listFrequency)
+            {
+                for (int i = 0; i < itemFrequency.Frequency; i++)
+                {
+                    result[resultIndex] = copy[itemFrequency.Index];
+                    resultIndex++;
+                }
             }
 
-            int[] CreateResult(List<ItemFrequency> listFrequency, int[] copy, int lentgh)
-            {
-                var result = new int[lentgh];
-                var resultIndex = 0;
-
-                foreach (var itemFrequency in listFrequency)
-                {
-                    for (int i = 0; i < itemFrequency.Frequency; i++)
-                    {
-                        result[resultIndex] = copy[itemFrequency.Index];
-                        resultIndex++;
-                    }
-                }
-
-                return result;
-            }
+            return result;
         }
 
         public static int GetIndexValue(this int[] input, int value)
         {
-            for(int i=0; i < input.Length; i++)
+            for (int i = 0; i < input.Length; i++)
             {
                 if (input[i] == value)
                 {
                     return i;
-                }                
+                }
             }
             return -1;
         }
 
-        public static (int[] result, int[] copy) SortCopy(this int[] input)
+        public static int[] SortIntArray(this int[] input)
         {
             var length = input.Length;
             bool didSwap;
-
-            var copy = new int[length];
-
-            for (int i=0; i< length; i++)
-            {
-                copy[i] = input[i];
-            }
 
             do
             {
@@ -125,7 +117,20 @@ namespace Algorithms_Data_Structures.Algorithms.SortByFrequency
                 }
             } while (didSwap);
 
-            return (input, copy);
+            return input;
+        }
+
+        public static int[] CreateCopyIntArray(this int[] input)
+        {
+            var length = input.Length;
+            var copy = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                copy[i] = input[i];
+            }
+
+            return copy;
         }
 
         public static void SortListByFrequency(this List<ItemFrequency> input)
@@ -141,23 +146,24 @@ namespace Algorithms_Data_Structures.Algorithms.SortByFrequency
                 {
                     if (input[i].Frequency < input[i + 1].Frequency)
                     {
-                        var temp = input[i];
-                        input[i] = input[i + 1];
-                        input[i + 1] = temp;
-
+                        input.SwapItems(i, i + 1);
                         didSwap = true;
                     }
                     else if (input[i].Frequency == input[i + 1].Frequency &&
-                        input[i].Index > input[i+1].Index)
+                        input[i].Index > input[i + 1].Index)
                     {
-                        var temp = input[i];
-                        input[i] = input[i + 1];
-                        input[i + 1] = temp;
-
+                        input.SwapItems(i, i + 1);
                         didSwap = true;
                     }
                 }
             } while (didSwap);
+        }
+
+        public static void SwapItems(this List<ItemFrequency> input, int index1, int index2)
+        {
+            var temp = input[index1];
+            input[index1] = input[index2];
+            input[index2] = temp;
         }
     }
 }
