b715fc427186d9c2e84ad62ba7003684ac5abb97
Roberto Freire <RobertoF@99x.io>
2024-05-23 18:55:25 -0300

    - add TwoPointers FindIndexOfPairSum

---
+++ Solution/Algorithms_Data_Structures/Algorithms/TwoPointers/TwoPointers.cs
@@ -0,0 +1,36 @@
+﻿namespace Algorithms_Data_Structures.Algorithms.TwoPointers;
+
+public static class TwoPointers
+{
+    // Given a sorted array A(sorted in ascending order), having N integers,
+    // find if there exists any pair of elements(A[i], A[j]) such that their sum is equal to X.
+
+    public static bool FindIndexOfPairSum(int[] array, int targetSum)
+    {
+        var leftPointer = 0;
+        var rightPointer = array.Length - 1;
+        var sum = 0;
+
+        do
+        {
+            sum = array[leftPointer] + array[rightPointer];
+            if (sum == targetSum)
+            {
+                return true;
+            }
+            
+            if (sum > targetSum)
+            {
+                rightPointer--;
+                continue;
+            }
+            else if (sum < targetSum)
+            {
+                leftPointer++;
+                continue;
+            }
+        } while (leftPointer < rightPointer);
+
+        return false;
+    }
+}
---
+++ Solution/tests/Algorithms/TwoPointersTests.cs
@@ -0,0 +1,31 @@
+﻿using Algorithms_Data_Structures.Algorithms.TwoPointers;
+using Xunit;
+
+namespace tests.Algorithms;
+
+public class TwoPointersTests
+{
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3, 4, 6 }, 6, true)]
+    [InlineData(new int[] { 1, 2, 3, 4, 6 }, 30, false)]
+    [InlineData(new int[] { 1, 2, 3, 4, 6 }, 0, false)]
+    [InlineData(new int[] { 1, 2, 3, 4, 6 }, -6, false)]
+    [InlineData(new int[] { 10, 20, 30, 40, 60 }, 33, false)]
+    [InlineData(new int[] { 1, 2, 3, 4, 6, 4, 2, 6 }, 10, true)]
+    [InlineData(new int[] { 1, 2, 3, 4, 6 }, 7, true)]
+    [InlineData(new int[] { -3, -1, 0, 2, 4, 5 }, 1, true)]
+    [InlineData(new int[] { -3, -1, 0, 2, 4, 5 }, -4, true)]
+    [InlineData(new int[] { 1, 2, 3, 4, 6 }, 12, false)] // No pair found
+    [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 15, true)]
+    [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 17, true)]
+    [InlineData(new int[] { -10, -5, 0, 5, 10, 15, 20 }, 5, true)]
+    [InlineData(new int[] { -10, -5, 0, 5, 10, 15, 20 }, 25, true)]
+    [InlineData(new int[] { -10, -5, 0, 5, 10, 15, 20 }, 30, true)] // No pair found
+    [InlineData(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, 2, true)] // Multiple same elements
+    [InlineData(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, 3, false)] // Multiple same elements, no valid pair
+    public void MustFindIndexesOfFirstPairSum(int[] array, int targetSum, bool expectedResult)
+    {
+        var result = TwoPointers.FindIndexOfPairSum(array, targetSum);
+        Assert.Equal(expectedResult, result);
+    }
+}
