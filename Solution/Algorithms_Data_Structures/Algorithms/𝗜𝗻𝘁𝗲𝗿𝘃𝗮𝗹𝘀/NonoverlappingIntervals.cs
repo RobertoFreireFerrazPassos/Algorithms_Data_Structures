e8bc679f3be2e681659f520f4c5cf91432e267d9
Roberto Freire <RobertoF@99x.io>
2024-05-24 08:15:56 -0300

    - add NonoverlappingIntervals

---
+++ Solution/Algorithms_Data_Structures/Algorithms/𝗜𝗻𝘁𝗲𝗿𝘃𝗮𝗹𝘀/NonoverlappingIntervals.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Algorithms_Data_Structures.Algorithms;
+
+public static class NonoverlappingIntervals
+{
+    public static int Solution(int[][] intervals)
+    {
+        var result = 0;
+        Array.Sort(intervals, (a,b) => a[0].CompareTo(b[0]));
+        int[] prevPointer = intervals[0];
+
+        for (int i = 1; i < intervals.Length; i++)
+        {
+            int[] currPointer = intervals[i];
+            if (IsOverlapping(prevPointer, currPointer))
+            {
+                result++;
+                if (prevPointer[1] >= currPointer[1])
+                {
+                    prevPointer = currPointer;
+                }
+            }
+            else
+            {
+                prevPointer = currPointer;
+            }
+        }
+
+        return result;
+    }
+
+    private static bool IsOverlapping(int[] a, int[]b)
+    {
+        return b[0] < a[1];
+    }
+}
---
+++ Solution/tests/Algorithms/NonoverlappingIntervalsTests.cs
@@ -0,0 +1,101 @@
+﻿using Algorithms_Data_Structures.Algorithms;
+using System.Collections.Generic;
+using Xunit;
+
+namespace tests.Algorithms;
+
+public class NonoverlappingIntervalsTests
+{
+    public static IEnumerable<object[]> GetTestData()
+    {
+        yield return new object[] {
+            new int[][] {
+                new int[] { 1, 2 },
+                new int[] { 2, 3 },
+                new int[] { 3, 4 },
+                new int[] { 1, 3 }
+            }, 1 };
+
+        yield return new object[] {
+            new int[][] {
+                new int[] { 1, 3 },
+                new int[] { 2, 3 },
+                new int[] { 3, 4 },
+                new int[] { 1, 2 }
+            }, 1 };
+
+        yield return new object[] {
+            new int[][] {
+                new int[] { 1, 2 },
+                new int[] { 1, 2 },
+                new int[] { 1, 2 }
+            }, 2 };
+
+        yield return new object[] {
+            new int[][] {
+                new int[] { 1, 2 },
+                new int[] { 2, 3 }
+            }, 0 };
+
+        yield return new object[] {
+            new int[][] {
+                new int[] { 1, 100 },
+                new int[] { 11, 22 },
+                new int[] { 1, 11 },
+                new int[] { 2, 12 }
+            }, 2 };
+
+        yield return new object[] {
+            new int[][] {
+                new int[] { 11, 22 },
+                new int[] { 1, 11 },
+                new int[] { 2, 12 },
+                new int[] { 1, 100 }
+            }, 2 };
+
+        yield return new object[] {
+            new int[][] {
+                new int[] { 1, 5 },
+                new int[] { 2, 3 },
+                new int[] { 3, 4 },
+                new int[] { 4, 5 }
+            }, 1 };
+
+        yield return new object[] {
+            new int[][] {
+                new int[] { -50, -25 },
+                new int[] { -30, -20 },
+                new int[] { -25, 0 }
+            }, 1 };
+
+        yield return new object[] {
+            new int[][] {
+                new int[] { 1, 10 },
+                new int[] { 2, 3 },
+                new int[] { 3, 4 },
+                new int[] { 5, 6 }
+            }, 1 };
+
+        yield return new object[] {
+            new int[][] {
+                new int[] { 1, 2 }
+            }, 0 };
+
+        yield return new object[] {
+            new int[][] {
+                new int[] { 1, 2 },
+                new int[] { 1, 3 },
+                new int[] { 1, 4 },
+                new int[] { 1, 5 }
+            }, 3 };
+    }
+
+    [Theory]
+    [MemberData(nameof(GetTestData))]
+    public void EraseOverlapIntervalsTests(int[][] intervals, int expected)
+    {
+        var result = NonoverlappingIntervals.Solution(intervals);
+
+        Assert.Equal(expected, result);
+    }
+}
