2ce8fe7c4789280d7ce39d07d4dc0fcd28ad0749
Roberto Freire <RobertoF@99x.io>
2024-06-01 16:28:08 -0300

    - add Reverse integer

---
+++ Solution/Algorithms_Data_Structures/Algorithms/reverserinteger/ReverserInteger.cs
@@ -0,0 +1,46 @@
+﻿namespace Algorithms_Data_Structures.Algorithms.reverserinteger;
+
+public class ReverserInteger
+{
+    public static string Reverse(int N)
+    {
+        var result = string.Empty;
+        int remaining = N;
+        var isZero= remaining % 10 == 0;
+        while (remaining != 0) {
+            if (isZero)
+            {
+                remaining= remaining/10;
+                isZero = remaining % 10 == 0;
+            }
+            else
+            {
+                result += remaining % 10;
+                remaining = remaining / 10;
+            }
+        }
+        return result;
+    }
+
+    public static string Reverse2(int N)
+    {
+        //1
+        var result = string.Empty;
+        var enable_print = N % 10 != 0;
+        while (N > 0)
+        {
+            if (N % 10 != 0)
+            {
+                enable_print = true;
+            }
+
+            if (enable_print)
+            {
+                result += N % 10;
+            }
+            N = N / 10;
+        }
+
+        return result;
+    }
+}
---
+++ Solution/tests/Algorithms/ReverserIntegerTest.cs
@@ -0,0 +1,37 @@
+﻿using Algorithms_Data_Structures.Algorithms.reverserinteger;
+using Xunit;
+
+namespace tests.Algorithms;
+
+public class ReverserIntegerTest
+{
+    [Theory]
+    [InlineData(123, "321")]
+    [InlineData(1230, "321")]
+    [InlineData(12300, "321")]
+    [InlineData(123000, "321")]
+    [InlineData(12030, "3021")]
+    [InlineData(10000, "1")]
+    [InlineData(10110, "1101")]
+    [InlineData(101100, "1101")]
+    public void MustReversesAInteger(int input, string expectedOutput)
+    {
+        string result = ReverserInteger.Reverse(input);
+        Assert.Equal(expectedOutput, result);
+    }
+
+    [Theory]
+    [InlineData(123, "321")]
+    [InlineData(1230, "321")]
+    [InlineData(12300, "321")]
+    [InlineData(123000, "321")]
+    [InlineData(12030, "3021")]
+    [InlineData(10000, "1")]
+    [InlineData(10110, "1101")]
+    [InlineData(101100, "1101")]
+    public void MustReversesAInteger_2(int input, string expectedOutput)
+    {
+        string result = ReverserInteger.Reverse2(input);
+        Assert.Equal(expectedOutput, result);
+    }
+}
